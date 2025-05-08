f5d5d4bb92126401123d8025e3bb44a5b945b574
David Bohman <david.bohman.dev@gmail.com>
2025-05-08 17:51:10 -0600

    refactor FoodMenu to work with updated App structure

--- snack-or-booze/src/FoodMenu.js
+++ snack-or-booze/src/FoodMenu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect, useParams } from "react-router-dom";
 import "./FoodMenu.css";
 import {
   Card,
@@ -10,22 +10,31 @@ import {
   ListGroupItem
 } from "reactstrap";
 
-function FoodMenu({ snacks }) {
+function FoodMenu({ allFood }) {
+  // Menu page - displays name of all items on the menu
+  const { foodMenu } = useParams();
+
+  // Ensure menu in url exist or redirect to 404
+  const food = allFood.find(f => f.menu === foodMenu);
+  if (!food) return <Redirect to="/404" />
+
   return (
     <section className="col-md-4">
       <Card>
         <CardBody>
           <CardTitle className="font-weight-bold text-center">
-            Food Menu
+            {food.title} Menu
           </CardTitle>
           <CardText>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </CardText>
           <ListGroup>
-            {snacks.map(snack => (
-              <Link to={`/snacks/${snack.id}`} key={snack.id}>
-                <ListGroupItem>{snack.name}</ListGroupItem>
+            {food.items.map(item => (
+              <Link to={`/${food.menu}/${item.id}`} key={`${food.menu}-${item.id}`}>
+                <ListGroupItem>
+                  {item.name}
+                </ListGroupItem>
               </Link>
             ))}
           </ListGroup>
