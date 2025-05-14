34a5e03a8446e292cafb5a2ec9a9d46cf8574085
David Bohman <david.bohman.dev@gmail.com>
2025-05-14 12:38:01 -0600

    move mock data to testingMockData for access by multiple test docs

--- snack-or-booze/src/App.test.js
+++ snack-or-booze/src/App.test.js
@@ -8,44 +8,9 @@ import '@testing-library/jest-dom';
 import { act } from 'react';
 import App from './App';
 import SnackOrBoozeApi from './Api';
+import { mockSnacksData, mockDrinksData } from './testingMockData';
 
 jest.mock('./Api.js');
-
-const mockSnacksData = [{
-    "id": "nachos",
-    "name": "Nachos",
-    "description": "An American classic!",
-    "recipe": "Cover expensive, organic tortilla chips with Cheez Whiz.",
-    "serve": "Serve in a hand-thrown ceramic bowl, garnished with canned black olives"
-},
-{
-    "id": "hummus",
-    "name": "Hummus",
-    "description": "Sure to impress your vegan friends!",
-    "recipe": "Purchase one container of hummus.",
-    "serve": "Place unceremoniously on the table, along with pita bread."
-}];
-const mockDrinksData = [{
-    "id": "martini",
-    "name": "Martini",
-    "description": "An ice-cold, refreshing classic.",
-    "recipe": "Mix 3 parts vodka & 1 part dry vermouth.",
-    "serve": "Serve very cold, straight up."
-},
-{
-    "id": "negroni",
-    "name": "Negroni",
-    "description": "A nice drink for a late night conversation.",
-    "recipe": "Mix equal parts of gin, Campari, and sweet vermouth.",
-    "serve": "Serve cold, either on the rocks or straight up."
-},
-{
-    "id": "gin-and-tonic",
-    "name": "Gin and Tonic",
-    "description": "Like regular tonic, but with gin.",
-    "recipe": "Mix 2 parts gin & 1 part tonic water.",
-    "serve": "Serve in a tall glass over ice, garnished with a lime wedge."
-}];
 
 describe('App', () => {
     it("renders without crashing", async () => {
@@ -59,7 +24,6 @@ describe('App', () => {
     });
 
     it("matches snapshot", async () => {
-        // mocking API responses
         SnackOrBoozeApi.getSnacks.mockResolvedValue(mockSnacksData);
         SnackOrBoozeApi.getDrinks.mockResolvedValue(mockDrinksData);
 
@@ -72,12 +36,10 @@ describe('App', () => {
         });
     });
 
-    it("loads and displays food quantities correctly", async () => {
-        // mocking API responses
+    it("loads homepage and displays food quantities correctly", async () => {
         SnackOrBoozeApi.getSnacks.mockResolvedValue(mockSnacksData);
         SnackOrBoozeApi.getDrinks.mockResolvedValue(mockDrinksData);
 
-        // Render the component
         render(<App />);
 
         // Initially, loading text should be in the document
@@ -89,5 +51,5 @@ describe('App', () => {
 
         // After loading is done, loading text should no longer be in the document
         expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
-    })
+    });
 })
---
+++ snack-or-booze/src/testingMockData.js
@@ -0,0 +1,38 @@
+
+const mockSnacksData = [{
+    "id": "nachos",
+    "name": "Nachos",
+    "description": "An American classic!",
+    "recipe": "Cover expensive, organic tortilla chips with Cheez Whiz.",
+    "serve": "Serve in a hand-thrown ceramic bowl, garnished with canned black olives"
+},
+{
+    "id": "hummus",
+    "name": "Hummus",
+    "description": "Sure to impress your vegan friends!",
+    "recipe": "Purchase one container of hummus.",
+    "serve": "Place unceremoniously on the table, along with pita bread."
+}];
+const mockDrinksData = [{
+    "id": "martini",
+    "name": "Martini",
+    "description": "An ice-cold, refreshing classic.",
+    "recipe": "Mix 3 parts vodka & 1 part dry vermouth.",
+    "serve": "Serve very cold, straight up."
+},
+{
+    "id": "negroni",
+    "name": "Negroni",
+    "description": "A nice drink for a late night conversation.",
+    "recipe": "Mix equal parts of gin, Campari, and sweet vermouth.",
+    "serve": "Serve cold, either on the rocks or straight up."
+},
+{
+    "id": "gin-and-tonic",
+    "name": "Gin and Tonic",
+    "description": "Like regular tonic, but with gin.",
+    "recipe": "Mix 2 parts gin & 1 part tonic water.",
+    "serve": "Serve in a tall glass over ice, garnished with a lime wedge."
+}];
+
+export { mockSnacksData, mockDrinksData };
